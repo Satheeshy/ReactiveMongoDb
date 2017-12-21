59f41619286c81d1166921a20ab12bb8333af4c2
Satheesh <satheeshyadava@git.com>
2017-12-21 14:19:01 -0600

    final

---
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -0,0 +1,94 @@
+package com.example.demo;
+import java.util.Arrays;
+import java.util.List;
+import java.util.UUID;
+
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+import org.springframework.data.mongodb.repository.ReactiveMongoRepository;
+import org.springframework.stereotype.Repository;
+
+@SpringBootApplication
+public class DemoApplication {
+
+	
+	@Bean
+	CommandLineRunner demo(MovieRepository movieRepository) {
+		CommandLineRunner runner;
+		runner = (args)->{
+		     movieRepository.deleteAll().subscribe();
+			List<String> list = Arrays.asList("hai","adsfas","sdfasdf","gasfddsa","asdghas");
+			list.stream().map(title -> new Movie(UUID.randomUUID().toString(),title,"romance"))
+			.map(movie -> movieRepository.save(movie))
+			.forEach(movie -> movie.subscribe(System.out::println));
+			
+		};
+		
+		return runner;
+	}
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoApplication.class, args);
+	
+	}
+	
+	
+	
+	
+}
+
+@Repository
+interface MovieRepository extends ReactiveMongoRepository<Movie, String>{
+	
+}
+
+
+@Document
+class Movie{
+	@Id
+	private String id;
+	
+	private String title,genre;
+
+	public Movie() {
+		// TODO Auto-generated constructor stub
+	}
+	public Movie(String id, String title, String genre) {
+		super();
+		this.id = id;
+		this.title = title;
+		this.genre = genre;
+	}
+
+	public String getId() {
+		return id;
+	}
+
+	public void setId(String id) {
+		this.id = id;
+	}
+
+	public String getTitle() {
+		return title;
+	}
+
+	public void setTitle(String title) {
+		this.title = title;
+	}
+
+	public String getGenre() {
+		return genre;
+	}
+
+	public void setGenre(String genre) {
+		this.genre = genre;
+	}
+	
+	
+	
+}
---
+++ src/main/java/com/example/demo/MovieController.java
@@ -0,0 +1,73 @@
+package com.example.demo;
+
+import javax.validation.Valid;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+
+@RestController
+public class MovieController {
+	
+	@Autowired
+	private MovieRepository movieRepository;
+	
+	@GetMapping("/movies")
+	public Flux<Movie> getAllMovies(){
+		return movieRepository.findAll();
+	}
+	
+	@PostMapping("/movies")
+	public Mono<Movie> createMovie(@Valid @RequestBody Movie movie)
+	{
+		return movieRepository.save(movie);
+	}
+	
+	@GetMapping("/movies{id}")
+	public Mono<ResponseEntity<Movie>> getMovieById(@PathVariable(value="id") String movieid){
+		return movieRepository.findById(movieid)
+				.map(m-> ResponseEntity.ok(m))
+				.defaultIfEmpty(ResponseEntity.notFound().build());
+	}
+	
+	@PutMapping("/movies/{id}")
+	public Mono<ResponseEntity<Movie>> updateTweet(@PathVariable String id,@Valid @RequestBody Movie m){
+		return movieRepository.findById(id)
+				.flatMap(movie -> {
+					movie.setTitle(m.getTitle());
+					return movieRepository.save(movie);
+				})
+				.map(movie -> new ResponseEntity<>(movie,HttpStatus.OK))
+				.defaultIfEmpty(new ResponseEntity<>(HttpStatus.NOT_FOUND));
+				
+				
+	}
+	@DeleteMapping("/movies/{id}")
+    public Mono<ResponseEntity<Void>> deleteMovie(@PathVariable(value = "id") String movieId) {
+		return movieRepository.findById(movieId)
+                .flatMap(movie ->
+                movieRepository.delete(movie)
+                            .then(Mono.just(new ResponseEntity<Void>(HttpStatus.OK)))
+                )
+                .defaultIfEmpty(new ResponseEntity<>(HttpStatus.NOT_FOUND));
+				
+	}
+	
+	@GetMapping(value = "/stream/movies", produces = MediaType.TEXT_EVENT_STREAM_VALUE)
+    public Flux<Movie> streamAllTweets() {
+        return movieRepository.findAll();
+    }
+	
+	
+}
